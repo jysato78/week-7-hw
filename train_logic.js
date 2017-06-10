20446712371c5e3d7e24c89544db409e317b9069
Jane Sato <Jane Sato>
2017-06-09 18:56:12 -0700

    final files

---
+++ train_logic.js
@@ -0,0 +1,100 @@
+
+$(document).ready(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBw7BfekX8sR4kAQQOE3ZwrIr0ZcCz29lI",
+    authDomain: "wk-7-trains.firebaseapp.com",
+    databaseURL: "https://wk-7-trains.firebaseio.com",
+    projectId: "wk-7-trains",
+    storageBucket: "wk-7-trains.appspot.com",
+    messagingSenderId: "1054883540282"
+  };
+  
+  firebase.initializeApp(config);
+
+  //Get a reference to the database service
+    var database = firebase.database();
+ 
+  // grab data from form
+  $("#submitBtn").on('click', function(){
+
+    // prevent form from submitting
+    event.preventDefault();
+
+    var train = $("#train-name").val().trim();
+    console.log("train name: ",train)
+
+    var destination = $("#destination").val().trim();
+    console.log("destination: ",destination)
+
+    var firstTrain = $("#first-train").val().trim();
+    console.log("first-train: ",firstTrain)
+    
+
+    var frequency = $("#frequency").val().trim();
+   
+
+    // Change what is saved in firebase
+      
+    var newTrain = {
+      train: train,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency
+    };
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+
+    // Alert
+    alert("Train successfully added");
+
+    //Clear the text boxes
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train").val("");
+    $("#frequency").val("");
+
+  }); 
+    // Firebase is always watching for changes to the data.
+    // When changes occurs it will print them to console and html
+    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+      // Print the initial data to the console.
+      console.log(childSnapshot.val());
+      
+      var convertedDate = moment(childSnapshot.val().firstTrain, 'hh:mm').subtract(1, 'years');
+      var myTrain = childSnapshot.val().train;
+      var myDestination = childSnapshot.val().destination;
+      var myFirstTrain = childSnapshot.val().firstTrain;
+      var tfrequency = childSnapshot.val().frequency;
+      
+
+
+      // pushed back 1 year to make sure it comes before current time
+      //var convertedDate = moment(childSnapshot.val().firstTrain, 'hh:mm').subtract(1, 'years');
+      var trainTime = moment(convertedDate).format('HH:mm');
+      var currentTime = moment();
+      // pushed back 1 year to make sure it comes before current time
+      var firstTimeConverted = moment(trainTime,'hh:mm').subtract(1, 'years');
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      var tRemainder = diffTime % tfrequency;
+      //solved
+      var tMinutesTillTrain = tfrequency - tRemainder;
+      //solved
+      var nextTrain = moment().add(tMinutesTillTrain, 'minutes').format('HH:mm')
+           
+      // Change the HTML
+      $("#tName").append("<div>" + myTrain);
+      $("#tDestination").append("<div>" + myDestination);
+      $("#tFrequency").append("<div>" + tfrequency + " min");
+      $("#tNext").append("<div>" + nextTrain);
+      $("#tHowLong").append("<div>" + tMinutesTillTrain + " min");
+      
+
+     
+      // If any errors are experienced, log them to console.
+    }, function(errorObject) {
+      console.log("The read failed: " + errorObject.code);
+          
+    });
+});
